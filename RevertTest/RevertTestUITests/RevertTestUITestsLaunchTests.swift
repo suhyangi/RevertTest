f712f3ff5655409347e9839323ded75800ac7d98
suhyangi <suhyangi@coupang.com>
2022-07-11 16:31:17 +0900

    Add Project

---
+++ RevertTest/RevertTestUITests/RevertTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RevertTestUITestsLaunchTests.swift
+//  RevertTestUITests
+//
+//  Created by suhyangi on 2022/07/11.
+//
+
+import XCTest
+
+class RevertTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
